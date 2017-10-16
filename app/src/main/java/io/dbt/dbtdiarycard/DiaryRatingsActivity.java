c0b495697fa8a128c42ba72fc931517b31fe665d
Amie Davis <amiedav@gmail.com>
2017-10-16 11:55:04 +1100

    initial commit

---
+++ app/src/main/java/io/dbt/dbtdiarycard/DiaryRatingsActivity.java
@@ -0,0 +1,13 @@
+package io.dbt.dbtdiarycard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiaryRatingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_ratings);
+    }
+}
